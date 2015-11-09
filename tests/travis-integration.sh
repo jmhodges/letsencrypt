0c3535a31df4c1d0ff137f7e63818558706097b1
Jeff Hodges <jeff@somethingsimilar.com>
2015-11-09 14:29:14 -0800

    okay then no more LE PATH

--- tests/travis-integration.sh
+++ tests/travis-integration.sh
@@ -4,6 +4,5 @@ set -o errexit
 
 source .tox/$TOXENV/bin/activate
 
-export LETSENCRYPT_PATH="$(pwd)/.."
 cd $GOPATH/src/github.com/letsencrypt/boulder/
 python test/integration-test.py --letsencrypt
